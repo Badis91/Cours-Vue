8a54f090b354f5d24ff7ef509835d880a972b944
Badis91 <146003225+Badis91@users.noreply.github.com>
2025-02-13 11:39:38 +0100

    Initial commit

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,18 @@
+<template>
+
+<button :id="data.id" :type="data.type ?? 'text'">Reinitialiser</button>
+
+</template>
+
+<script setup lang="ts">
+
+interface ButtonComponentProperties {
+    data: {
+        id: string,
+        type: 'button' | 'submit' | 'reset',
+    }
+}
+
+defineProps<ButtonComponentProperties>()
+
+</script>
---
+++ src/components/FormComponent.vue
@@ -0,0 +1,9 @@
+
+<template>
+<form class="login-form">
+    
+
+
+    
+</form>
+</template>
